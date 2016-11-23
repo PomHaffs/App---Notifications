2e81b967e6f84d14f2039801b9600d189f7a49b7
PomHaffs <tomashaffenden@icloud.com>
2016-11-23 15:02:59 +1100

    Added UI

--- Notifications/ViewController.swift
+++ Notifications/ViewController.swift
@@ -6,20 +6,85 @@
 //  Copyright © 2016 PomHaffs. All rights reserved.
 //
 
+//Permision needed to us NOTIFICATIONS so need to do the p.list thing
+
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 1. Request permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+        
+            if granted {
+                print("notification access granted!")
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func notifyButtonPressed(sender: UIButton) {
+        
     }
-
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These are great"
+        notif.body = "The new notification options is iOS 10 are what I've always dreamed of!"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error!)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
